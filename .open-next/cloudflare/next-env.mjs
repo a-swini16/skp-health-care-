07d3bde88f17a5376274e8c741978d5716d8e6c3
Aswini <aswini@example.com>
2026-07-17 19:09:19 +0530

    Upgrade Next.js to 15 to bypass Cloudflare 14.2.35 enforcement

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_SUPABASE_URL":"https://ggxnwihqpaoystbejrax.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_R_7MhH3YlzfPtfW6tK-pmw_m1xThEy6"};
+export const development = {"NEXT_PUBLIC_SUPABASE_URL":"https://ggxnwihqpaoystbejrax.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_R_7MhH3YlzfPtfW6tK-pmw_m1xThEy6"};
+export const test = {};
